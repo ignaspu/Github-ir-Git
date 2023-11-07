30a9e8f7fb75e8a084cf41c75be808ba7b1be2f9
ignaspu <pupelis.ignas@gmail.com>
2023-11-07 16:01:18 +0200

    pabaigtas mobile ir desktop intro

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ document
   e.preventDefault();
   document.querySelector('header').style.display = "none";
   document.querySelector('#intro').style.display = "unset";
-  
+});
+document
+.querySelector('#xIcon')
+.addEventListener('click', e => {
+  e.preventDefault();
+  document.querySelector('header').style.display = "block";
+  document.querySelector('#intro').style.display = "unset";
 });
